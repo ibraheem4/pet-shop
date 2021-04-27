0c0d81f519950260cf19cbe4697a24d3b146a78c
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2021-04-27 18:39:20 -0400

    Add additional tests for return

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -20,28 +20,41 @@ contract TestAdoption is Ownable {
   function testUserCanAdoptAnimal() public {
     uint adoptedAnimalId = adoption.adoptAnimal(expectedAnimalId);
 
-    Assert.equal(adoptedAnimalId, expectedAnimalId, "Adoption of the expected animal should match what is returned.");
+    Assert.equal(adoptedAnimalId, expectedAnimalId, "adoptAnimal should return the id of the expected animal");
   }
 
   // Testing retrieval of a single animal's owner
   function testGetAdopterAddressByAnimalId() public {
-   address animalAdopter = adoption.animalAdopters(expectedAnimalId);
+    address animalAdopter = adoption.animalAdopters(expectedAnimalId);
 
-   Assert.equal(animalAdopter, expectedAnimalAdopter, "Owner of the expected animal should be this contract");
+    Assert.equal(animalAdopter, expectedAnimalAdopter, "Owner of the expected animal should be this contract");
   }
 
   // Testing retrieval of all animal owners
   function testGetAdopterAddressByAnimalIdInArray() public {
-   // Store adopters in memory rather than contract's storage
-   address[16] memory animalAdopters = adoption.getAnimalAdopters();
+    // Store adopters in memory rather than contract's storage
+    address[16] memory animalAdopters = adoption.getAnimalAdopters();
 
-   Assert.equal(animalAdopters[expectedAnimalId], expectedAnimalAdopter, "Owner of the expected animal should be this contract");
+    Assert.equal(animalAdopters[expectedAnimalId], expectedAnimalAdopter, "Owner of the expected animal should be this contract");
   }
 
   // Testing the returnAnimal() function
   function testUserCanReturnAnimal() public {
     uint returnedAnimalId = adoption.returnAnimal(expectedAnimalId);
 
-    Assert.equal(returnedAnimalId, expectedAnimalId, "Returning the expected animal should match what is returned.");
+    Assert.equal(returnedAnimalId, expectedAnimalId, "returnAnimal should return the id of the expected animal");
+  }
+
+  // Testing retrieval of a single animal's owner
+  function testGetReturnerAddressByAnimalId() public {
+    address animalAdopter = adoption.animalAdopters(expectedAnimalId);
+
+    Assert.equal(animalAdopter,  address(0x0), "Owner of the expected animal is reset to the default address");
+  }
+
+  function testGetReturnerAddressByAnimalIdInArray() public {
+    // Store adopters in memory rather than contract's storage
+    address[16] memory animalAdopters = adoption.getAnimalAdopters();
+    Assert.equal(animalAdopters[expectedAnimalId], address(0x0), "Owner of the expected animal is reset to the default address");
   }
 }
